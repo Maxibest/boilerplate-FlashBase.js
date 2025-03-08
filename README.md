1bd7180f5839cf98525c002633b44a4cc62440af
Kaïto <yohann.gonthier.jr@gmail.com>
2025-03-08 11:31:43 +0100

    Boilerplate

---
+++ README.md
@@ -0,0 +1,2 @@
+## Installation
+`npm install react-router-dom`
---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import '../css/index.css'
+
+const Home = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="./FLash.jpg" className="App-logo" alt="logo" />
+        <p>⚡⚡⚡</p>
+      </header>
+    </div>
+  );
+};
+
+export default Home;
